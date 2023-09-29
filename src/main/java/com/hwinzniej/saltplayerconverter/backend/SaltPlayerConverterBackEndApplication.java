96e7c882bd5625f6a6079ffbd53f0ee1e4d2ac5c
Winnie0408 <1196903869@qq.com>
2023-09-29 14:48:25 +0800

    初次提交 Initial commit

---
+++ src/main/java/com/hwinzniej/saltplayerconverter/backend/SaltPlayerConverterBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.hwinzniej.saltplayerconverter.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaltPlayerConverterBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaltPlayerConverterBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hwinzniej/saltplayerconverter/backend/SaltPlayerConverterBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwinzniej.saltplayerconverter.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaltPlayerConverterBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
